f4352a8cd6b50180d3b06e01076f39add3894c56
troupeoperations <troupeoperations@pistons.dreamhost.com>
2013-11-03 09:56:04 -0800

    adding placeholder old site

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style type="text/css">
+	body {
+		background-image: url("images/lghtmesh.png");
+	}
+	a {
+		color: #666;
+	}
+	a:hover {
+		color: black;
+	}
+	.center-width { 
+		width: 576px;
+		margin-left: auto;
+		margin-right: auto;
+		text-align: center;
+	}
+	.center-height {
+		position: absolute;
+		height: 200px;
+		top: 50%;
+		margin-top: -100px;	
+		text-align: center;
+	}
+</style>
+</head>
+<body>
+<div class="center-width">
+	<div class="center-height">
+		<img src="images/placeholder_logo2.png">
+		<div>
+			<a href="mailto:troupe@troupeoperations.com"><img src="images/placeholder_email.png"></a>
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32115593-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</body>
+</html>
+
