60d43148d378ba2b79ae3d31fc5eb77f897e80c9
troupeoperations <troupeoperations@pistons.dreamhost.com>
2013-11-03 10:41:22 -0800

    adding bootstrap

--- index.php
+++ index.php
@@ -1,40 +1,53 @@
 <!DOCTYPE html>
-<html>
-<head>
-<style type="text/css">
-	body {
-		background-image: url("images/lghtmesh.png");
-	}
-	a {
-		color: #666;
-	}
-	a:hover {
-		color: black;
-	}
-	.center-width { 
-		width: 576px;
-		margin-left: auto;
-		margin-right: auto;
-		text-align: center;
-	}
-	.center-height {
-		position: absolute;
-		height: 200px;
-		top: 50%;
-		margin-top: -100px;	
-		text-align: center;
-	}
-</style>
-</head>
-<body>
-<div class="center-width">
-	<div class="center-height">
-		<img src="images/placeholder_logo2.png">
-		<div>
-			<a href="mailto:troupe@troupeoperations.com"><img src="images/placeholder_email.png"></a>
-		</div>
-	</div>
-</div>
+<html lang="en">
+  <head>
+    <title>Troupe Operations</title>
+    <meta troupe-name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/troupe.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+   <div class="container span8"> 
+
+    <div class="row">
+        <div class="span3">
+            <img src="images/placeholder_logo2.png">
+        </div>
+        <div class="span5">
+            <div class="troupe-name"><h3><a href="http://dan.taeyounglee.com">Dan Taeyoung</a></h3></div>
+            <div class="troupe-name"><h3><a href="http://issuu.com/yvonnedemitrakonstantinidis/docs/ydkonstantinidis_refuse">Yvonne-Demitra Konstantinidis</a></h3></div>
+            <div class="troupe-name"><h3>Eivind Karlsen</h3></div>
+        </div>
+    </div>
+
+   </div>
+    <!--
+                <a href="mailto:troupe@troupeoperations.com"><img src="images/placeholder_email.png"></a>
+    <img src="images/placeholder_logo2.png">
+-->
+
+<?php /*
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=9378805; 
+var sc_invisible=1; 
+var sc_security="ab1adda5"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="web analytics"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/9378805/0/ab1adda5/1/"
+alt="web analytics"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
 
 
 <script type="text/javascript">
@@ -50,6 +63,7 @@
   })();
 
 </script>
-</body>
+*/ ?>
+  </body>
 </html>
 
